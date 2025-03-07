680ff0bbc8e6c1b921585edd99c8d6a69b06fb0d
rexmech6n <cm0bykr@gmail.com>
2025-03-07 09:18:36 +0300

    Add ClimberSubsystem

---
+++ src/main/java/com/celestial/subsystems/ClimberSubsystem.java
@@ -0,0 +1,33 @@
+package com.celestial.subsystems;
+
+import com.revrobotics.spark.SparkBase;
+import com.revrobotics.spark.SparkLowLevel;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.SparkBaseConfig;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class ClimberSubsystem extends SubsystemBase {
+    private SparkMax climberMotor = new SparkMax(20, SparkLowLevel.MotorType.kBrushless);
+
+    public ClimberSubsystem() {
+        SparkMaxConfig config = new SparkMaxConfig();
+        config.smartCurrentLimit(23);
+        config.idleMode(SparkBaseConfig.IdleMode.kBrake);
+
+        climberMotor.configure(config, SparkBase.ResetMode.kResetSafeParameters, SparkBase.PersistMode.kPersistParameters);
+    }
+
+    public void setSpeed(double speed) {
+        climberMotor.set(speed);
+    }
+
+    public Command controlClimberCommand(double speed) {
+        return run(() -> {
+           setSpeed(speed);
+        }).finallyDo(() -> {
+            setSpeed(0);
+        });
+    }
+}
